e4bc923902284529b8011cf63977ac1843b415dc
choiyebeen <dpqlsdl8@naver.com>
2023-10-19 20:44:39 +0900

    1019

--- Choiyebeen/ViewModel/MainViewModel.cs
+++ Choiyebeen/ViewModel/MainViewModel.cs
@@ -5,16 +5,23 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 using Choiyebeen.Model;
 using Choiyebeen.Repositories;
+using FontAwesome.Sharp;
 
 namespace Choiyebeen.ViewModel
 {
    public class MainViewModel:ViewModelBase
     {
         private UserAccountModel _currentUserAccount;
+        private ViewModelBase _currentChildView;
+        private string _caption;
+        private IconChar _icon;
+
         private IUserRepository userRepository;
 
+        //properties
         public UserAccountModel CurrentUserAccount
         {
             get
@@ -29,11 +36,79 @@ namespace Choiyebeen.ViewModel
             }
         }
 
+        public ViewModelBase CurrentChildView
+        {
+            get
+            {
+                return _currentChildView;
+            }
+
+            set
+            {
+                _currentChildView = value;
+                OnPropertyChanged(nameof(CurrentChildView));
+            }
+        }
+
+        public string Caption
+        {
+            get
+            {
+                return Caption;
+            }
+
+            set
+            {
+                _caption = value;
+                OnPropertyChanged(nameof(Caption));
+            }
+        }
+
+        public IconChar Icon
+        {
+            get
+            {
+                return _icon;
+            }
+
+            set
+            {
+                _icon = value;
+                OnPropertyChanged(nameof(Icon));
+            }
+        }
+        
+        //--> commands
+        public ICommand ShowHomeViewCommand { get; }
+        public ICommand ShowCustomerViewCommand { get; }
+
         public MainViewModel()
         {
             userRepository = new UserRepository();
             CurrentUserAccount = new UserAccountModel();
+
+            //Initialize commands
+            ShowHomeViewCommand = new ViewModelCommand(ExecuteShowHomeViewCommand);
+            ShowCustomerViewCommand = new ViewModelCommand(ExecuteShowCustomerViewCommand);
+
+            //Default view
+            ExecuteShowHomeViewCommand(null);
+
             LoadCurrentUserData();
+        }
+
+        private void ExecuteShowCustomerViewCommand(object obj)
+        {
+            CurrentChildView = new CustomerViewModel();
+            Caption = "Customers";
+            Icon = IconChar.UserGroup;
+        }
+
+        private void ExecuteShowHomeViewCommand(object obj)
+        {
+            CurrentChildView = new HomeViewModel();
+            Caption = "Dashboard";
+            Icon = IconChar.Home;
         }
 
         private void LoadCurrentUserData()
@@ -43,7 +118,7 @@ namespace Choiyebeen.ViewModel
             {
                 
                     CurrentUserAccount.Username = user.Username;
-                    CurrentUserAccount.DisplayName = $"Welcome {user.Name} {user.LastName};)";
+                    CurrentUserAccount.DisplayName = $" {user.Name} {user.LastName}";
                     CurrentUserAccount.ProfilePicture = null;
               
             }
